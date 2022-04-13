1f9255d81a779abca9c5b61a99b834a1cf99877e
Ulistonee <ulistonee@gmail.com>
2022-04-13 18:12:23 +0300

    working on models

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -5,5 +5,8 @@ def index(request):
     context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
     return render(request, 'rango/index.html', context=context_dict)
 
+
 def about(request):
-    return HttpResponse("<a href='/rango/'>Index</a>")
+    context_dict = {'my_name': "Aizhan"}
+    return render(request, 'rango/about.html', context=context_dict)
+
